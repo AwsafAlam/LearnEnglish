7a7dc33673963c9d8331721d168aef3d52fb018c
AwsafAlam <1505114.maaa@ugrad.cse.buet.ac.bd>
2018-10-07 03:45:55 +0600

    Added Timeline View

---
+++ app/src/main/java/com/example/awsaf/learnenglish/utils/TimeLineViewHolder.java
@@ -0,0 +1,32 @@
+package com.example.awsaf.learnenglish.utils;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.awsaf.learnenglish.R;
+import com.github.vipulasri.timelineview.TimelineView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+/**
+ * Created by HP-HP on 05-12-2015.
+ */
+public class TimeLineViewHolder extends RecyclerView.ViewHolder {
+
+    @BindView(R.id.text_timeline_date)
+    TextView mDate;
+    @BindView(R.id.text_timeline_title)
+    TextView mMessage;
+    @BindView(R.id.time_marker)
+    TimelineView mTimelineView;
+
+    public TimeLineViewHolder(View itemView, int viewType) {
+        super(itemView);
+
+        ButterKnife.bind(this, itemView);
+        mTimelineView.initLine(viewType);
+    }
+}
--- app/src/main/java/com/example/awsaf/learnenglish/view/MainActivity.java
+++ app/src/main/java/com/example/awsaf/learnenglish/view/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.awsaf.learnenglish.view;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,9 +16,23 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import com.example.awsaf.learnenglish.R;
+import com.example.awsaf.learnenglish.model.OrderStatus;
+import com.example.awsaf.learnenglish.model.Orientation;
+import com.example.awsaf.learnenglish.model.TimeLineModel;
+import com.example.awsaf.learnenglish.utils.TimeLineAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+
+    private RecyclerView mRecyclerView;
+    private TimeLineAdapter mTimeLineAdapter;
+    private List<TimeLineModel> mDataList = new ArrayList<>();
+    private Orientation mOrientation;
+    private boolean mWithLinePadding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,14 +41,14 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -42,7 +58,62 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        mOrientation = Orientation.VERTICAL;
+        mWithLinePadding = false;
+//        setTitle(mOrientation == Orientation.HORIZONTAL ? getResources().getString(R.string.horizontal_timeline) : getResources().getString(R.string.vertical_timeline));
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        mRecyclerView.setLayoutManager(getLinearLayoutManager());
+        mRecyclerView.setHasFixedSize(true);
+
+        initView();
+
     }
+
+    private LinearLayoutManager getLinearLayoutManager() {
+        if (mOrientation == Orientation.HORIZONTAL) {
+            return new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        } else {
+            return new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        }
+    }
+
+    private void initView() {
+        setDataListItems();
+        mTimeLineAdapter = new TimeLineAdapter(mDataList, mOrientation, mWithLinePadding);
+        mRecyclerView.setAdapter(mTimeLineAdapter);
+    }
+
+    private void setDataListItems(){
+        mDataList.add(new TimeLineModel("Item successfully delivered", "", OrderStatus.INACTIVE));
+        mDataList.add(new TimeLineModel("Courier is out to delivery your order", "2017-02-12 08:00", OrderStatus.ACTIVE));
+        mDataList.add(new TimeLineModel("Item has reached courier facility at New Delhi", "2017-02-11 21:00", OrderStatus.COMPLETED));
+        mDataList.add(new TimeLineModel("Item has been given to the courier", "2017-02-11 18:00", OrderStatus.COMPLETED));
+        mDataList.add(new TimeLineModel("Item is packed and will dispatch soon", "2017-02-11 09:30", OrderStatus.COMPLETED));
+        mDataList.add(new TimeLineModel("Order is being readied for dispatch", "2017-02-11 08:00", OrderStatus.COMPLETED));
+        mDataList.add(new TimeLineModel("Order processing initiated", "2017-02-10 15:00", OrderStatus.COMPLETED));
+        mDataList.add(new TimeLineModel("Order confirmed by seller", "2017-02-10 14:30", OrderStatus.COMPLETED));
+        mDataList.add(new TimeLineModel("Order placed successfully", "2017-02-10 14:00", OrderStatus.COMPLETED));
+    }
+
+//    @Override
+//    protected void onSaveInstanceState(Bundle savedInstanceState) {
+//        if(mOrientation!=null)
+//            savedInstanceState.putSerializable(MainActivity.EXTRA_ORIENTATION, mOrientation);
+//        super.onSaveInstanceState(savedInstanceState);
+//    }
+//
+//    @Override
+//    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+//        if (savedInstanceState != null) {
+//            if (savedInstanceState.containsKey(MainActivity.EXTRA_ORIENTATION)) {
+//                mOrientation = (Orientation) savedInstanceState.getSerializable(MainActivity.EXTRA_ORIENTATION);
+//            }
+//        }
+//        super.onRestoreInstanceState(savedInstanceState);
+//    }
+
 
     @Override
     public void onBackPressed() {
