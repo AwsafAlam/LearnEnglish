47876bd9bb37ea43f93102d57d6aa75890480f8f
AwsafAlam <1505114.maaa@ugrad.cse.buet.ac.bd>
2018-10-07 02:35:03 +0600

    Added Login Screen

---
+++ app/src/main/java/com/example/awsaf/learnenglish/view/LoginActivity.java
@@ -0,0 +1,29 @@
+package com.example.awsaf.learnenglish.view;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.awsaf.learnenglish.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        login = findViewById(R.id.button_signin);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this , MainActivity.class));
+            }
+        });
+    }
+}
